b071f9ca54a5dbd7532206f6dc82f825ca1efafe
lucastex <lucastex@gmail.com>
2011-05-29 09:56:56 -0300

    checking text to stamp

--- grails-app/controllers/stampyourpdf/PublicController.groovy
+++ grails-app/controllers/stampyourpdf/PublicController.groovy
@@ -8,12 +8,13 @@ class PublicController {
 
 	def upload = {
 		
+		def text = params.text
 		def file = request.getFile('file')
-		if (file) {
+		if (file && text) {
 			
 			def extension = file.extension
 			def newFilename = "stamped-${file.originalFilename.slug()}.${extension}"
-			def outputStream = stampService.stamp(file.inputStream, params.text)
+			def outputStream = stampService.stamp(file.inputStream, text)
 			response.setContentType("application/pdf")
 			response.setHeader("Content-disposition", "attachment; filename=${newFilename}")
 			outputStream.writeTo(response.outputStream)
