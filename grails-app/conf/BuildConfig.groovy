46a9792809fd3c47164dc0772e66461392592e65
lucastex <lucastex@gmail.com>
2011-05-29 10:20:40 -0300

    cleaned buildconfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,35 +1,25 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+
     inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+    log "warn"
+
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
         mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
+
     dependencies {
-	
         compile 'com.lowagie:itext:2.0.7'
     }
 
 	plugins {
-		
 		compile ':jquery:1.6.1.1'
 		compile ':resources:1.0'
 		compile ':zipped-resources:1.0'
